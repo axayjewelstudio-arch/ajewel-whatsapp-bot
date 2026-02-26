c82a716978a101679249df238810ef1d2f42a690
axayjewelstudio-arch <axayjewelstudio@gmail.com>
2026-02-26 11:57:20 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
+from flask import Flask
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
 
-token = os.getenv("META_SYSTEM_USER_TOKEN")
-print(token)
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Hello World"
+
+if __name__ == '__main__':
+    app.run()
